e9140e30de85d191c73eb2ebbb13fa7d7469c353
Soheil Novinfard <novinfard@gmail.com>
2017-12-10 19:55:52 +0000

    Add 3rd Script
    Who is the most profitable client?

---
+++ script3.js
@@ -0,0 +1,13 @@
+db.bookings.aggregate(
+   [
+     {
+       $group:
+         {
+           _id: {clientId: '$clientId'},
+           totalProfit: { $sum: '$amount' },
+         }
+     },
+     {$sort: {'totalProfit': -1} },
+     {$limit: 1}
+   ]
+)
