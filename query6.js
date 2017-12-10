49ebda00377ca18c7e77ead49b09cab6ab88e237
Soheil Novinfard <novinfard@gmail.com>
2017-12-10 18:40:25 +0000

    Add 6th Query
    Find the most expensive bookings in some period (use sort here, use
    .limit(10) for instance)

---
+++ query6.js
@@ -0,0 +1,12 @@
+// first way
+db.bookings.aggregate(
+   [
+     { $sort : {amount  : -1, dateOfPayment: -1} },
+     { $limit: 10}
+   ]
+)
+   
+// second way  
+db.bookings.find()
+     .sort({amount  : -1, dateOfPayment: -1} )
+     .limit(10);
