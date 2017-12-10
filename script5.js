11aa12ed91e0f830f350655b226c3851261d053d
Soheil Novinfard <novinfard@gmail.com>
2017-12-10 19:53:59 +0000

    Fixed - Wrong submission

--- script5.js
+++ script5.js
@@ -1,32 +1,3 @@
-// Total last month revenue
-db.revenue.aggregate(
-   [{
-       $match: {
-            'startDate': {$gte: new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1, 0 , 0, 0)},
-           'endDate': {$lte: new Date(new Date().getFullYear(), new Date().getMonth(), 0, 23, 59, 59)},
-       }
-     },
-     {
-       $group:
-         {
-           "_id": null,
-           'totalLastMonthRevenue': { $sum: '$driverWage' },
-         }
-     }
-   ]
-)
-     
-// Average revenue
-db.revenue.aggregate(
-   [
-     {
-       $group:
-         {
-           "_id": null,
-           "minDate": {$min: '$startDate'},
-           "maxDate": {$max: '$endDate'},
-           'totalLastMonthRevenue': { $sum: '$driverWage' },
-         }
-     }
-   ]
-);
+var pageNumber = 1;
+var limitPerPage = 10;
+db.bookings.find().skip(limitPerPage * (pageNumber - 1) ).limit(limitPerPage)
