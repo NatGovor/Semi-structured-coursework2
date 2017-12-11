5d7be5219c2e380e76302c9edf2e7cde194387d4
Natalia Govor <ngovor93@gmail.com>
2017-12-11 14:07:04 +0000

    Rename queries numbers as suggested in the document query 5 was removed

--- queries/query6.js
+++ queries/query6.js
@@ -1,10 +1,26 @@
-// first way
-db.bookings.aggregate([
-	{ $sort : {amount  : -1, dateOfPayment: -1} },
-    { $limit: 10 }
-]);
-   
-// second way  
-db.bookings.find()
-     .sort({amount  : -1, dateOfPayment: -1} )
-     .limit(10);
+// first method
+// set the necessary date interval
+db.bookings.find({
+    'dateTime': {
+        $gte: ISODate("2017-11-01T00:00:00.000Z"), 
+        $lte: ISODate("2017-11-01T23:59:59.000Z")
+        }
+}).count();
+
+// second method
+// set the necessary date interval
+db.bookings.aggregate(
+  [
+    {
+      $match: {
+        'dateTime': {
+            $gte: ISODate("2017-11-01T00:00:00.000Z"), 
+            $lte: ISODate("2017-11-01T23:59:59.000Z")
+        }
+      }
+    },
+    {
+      $count: "todayBookingCount"
+    }
+  ]
+);
