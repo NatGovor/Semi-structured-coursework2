a2c525f12e4bcb40a427c457768c4e36fe02f116
Natalia Govor <ngovor93@gmail.com>
2017-12-11 13:03:49 +0000

    Finalize queries 1, 2 and 4

--- query2.js
+++ query2.js
@@ -1,7 +1,12 @@
-db.getCollection('bookings').find({
-    'dateTime': {
-        $gte: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()), 
-        $lte: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 59, 59)
-        }, 
-    'regularBookingId': {"$exists":true, $ne: null},
-})
+// find regular bookings which may happen in the specified day
+// regularType is unstructured field which makes it hard to anaylize only with querry. We assume that it will be analyzed by some program or operator
+db.getCollection('regularBookings').find({
+    'subscriptionStartDate': { $lte: ISODate("2017-11-20T00:00:00.000Z")},
+    'subscriptionEndDate': { $gte: ISODate("2017-12-11T00:00:00.000Z")}
+});
+
+// find regular bookings which may happen today
+db.getCollection('regularBookings').find({
+    'subscriptionStartDate': { $lte: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())},
+    'subscriptionEndDate': { $gte: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 59, 59)}
+});
--- query4.js
+++ query4.js
@@ -1,6 +1,19 @@
-db.getCollection('bookings').aggregate({ 
-    $match: {'dateOfPayment': {"$exists":false, $eq: null}} },
-    { $group: 
-        { _id : 'totalUnpaidAmount', sum : { $sum: "$amount" } 
+// hom many unpaid rides(bookings) we have?
+db.getCollection('bookings').find({
+    'dateOfPayment': { $exists: false }
+}).count();
+
+// get all unpaid rides(bookings)
+db.getCollection('bookings').find({
+    'dateOfPayment': { $exists: false }
+});
+
+// how much money should we recieve from unpaid bookings?
+db.getCollection('bookings').aggregate(
+    { 
+        $match: {'dateOfPayment': { $exists: false }}
+    },
+    {
+        $group: { _id : 'totalUnpaidAmount', sum : { $sum: "$amount" }}
     } 
-})
+);
